212d81c58dcda0bf3dface8872f34a4cbc946227
Guillaume Gst <guillaume.gustin.9@gmail.com>
2025-02-21 21:48:27 +0100

    chore: Configure build to use public directory

--- astro.config.ts
+++ astro.config.ts
@@ -28,6 +28,9 @@ export default defineConfig({
  experimental: {
    clientPrerender: true
  },
+ redirects: {
+   '/admin': '/admin/index.html'
+ },
  viewTransitions: true,
  integrations: [
    tailwind({
@@ -143,6 +146,6 @@ export default defineConfig({
      target: 'node'
    },
    assetsInclude: ['**/*.otf', '**/*.ttf', '**/*.woff', '**/*.woff2'],
-   publicDir: 'src/assets',
+   publicDir: 'public',
  },
 });

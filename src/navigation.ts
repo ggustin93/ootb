c6221bb2aaa409f0d39e5bbd7b8adbe9fe28ed72
Guillaume Gst <guillaume.gustin.9@gmail.com>
2025-02-27 20:56:30 +0100

    refactor: Reorder festival navigation menu items

--- src/navigation.ts
+++ src/navigation.ts
@@ -27,12 +27,12 @@ export const headerData = {
           href: getPermalink('/festival#themes'),
         },
         {
-          text: 'Partenaires',
-          href: getPermalink('/festival#partenaires'),
-        },
-        {
           text: 'Informations pratiques',
           href: getPermalink('/festival#accessibility'),
+        },
+        {
+          text: 'Partenaires',
+          href: getPermalink('/festival#partenaires'),
         }
       ]
     },
